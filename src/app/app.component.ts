5f26b68d9a8a882c50df9f54705877555ed3bcbd
lubnashaikh123 <128475990+lubnashaikh123@users.noreply.github.com>
2023-08-03 01:12:04 +0530

    Table & List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iteam } from './shared/models/Team';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'List-Example';
+  team :Array<Iteam>=[
+    {           fname:"Shikhar", 
+                lname:"Dhavan",
+            role:"Top-order batsman",
+            batting:"left-Handed",
+            age:34,
+          
+    },
+    {
+                fname:"virat", 
+                lname:"kohli",
+            role:"Top-order batsman",
+            batting:"right-Handed",
+            age:34,
+                     
+    },		  
+    
+    {
+                fname:"Hardik", 
+                lname:"pandya",
+            role:"Top-order batsman",
+            batting:"right-Handed",
+            age:34,
+          
+    },
+    {
+                fname:"Mohammad", 
+                lname:"siraj",
+            role:"Bowler",
+            batting:"right-Handed",
+            age:31,
+          
+    },		  
+    ];
 }
